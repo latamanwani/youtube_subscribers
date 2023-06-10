aa201d51e99ca81583b14e2990ff2ac0f56ef302
latamanwani <131308359+latamanwani@users.noreply.github.com>
2023-06-10 15:21:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const dbUrl =  "mongodb+srv://<lata007manwani>:<LqMrCOPxMSPwGhLk>@subscribers.v1keo1i.mongodb.net/";
+const dbUrl =  "mongodb+srv://<lata007manwani>:<LqMrCOPxMSPwGhL>@subscribers.v1keo1i.mongodb.net/?retryWrites=true&w=majority";
 mongoose
   .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
